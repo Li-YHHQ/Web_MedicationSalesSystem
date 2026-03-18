1a29fcea942672f972b3a138fd037d567ab2ff99
李松伦 <1075793067@qq.com>
2026-03-18 14:30:36 +0800

    监控：全局异常处理器接入Sentry异常上报

--- src/main/java/com/neusoft/coursemgr/exception/GlobalExceptionHandler.java
+++ src/main/java/com/neusoft/coursemgr/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import io.sentry.Sentry;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.slf4j.Logger;
@@ -49,6 +50,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ApiResponse<Void> handleOther(Exception ex) {
         log.error("Unhandled exception", ex);
+        Sentry.captureException(ex);
         return ApiResponse.fail("服务器内部错误");
     }
 }
